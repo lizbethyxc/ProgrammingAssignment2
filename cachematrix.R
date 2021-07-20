1ad9efb85325ce0e2c992f1c1e173d56df580bf3
lizbethyxc <72246699+lizbethyxc@users.noreply.github.com>
2021-07-20 17:39:05 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,16 @@
+## [Put comments here that describe what your functions do]
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## There are two functions makeCacheMatrix, cacheSolve
-## makeCacheMatrix cosist of set, get, setinv, getinv
-makeCacheMatrix <- function(x = matrix()) {       # This function creates a special "matrix" object that can cache its inverse
-     inv <- NULL                                  # initialize inv as NULL
-     set <- function(y){                          # define the set function to assign new
-          x <<- y
-          m <<- NULL
-     }
-     get <- function()x                           # function to get matrix x
-     setinv <- function(inverse) inv <<- inverse  # assigns value of inv in parent environment
-     getinv <- function(){
-          inver <- ginv(x)
-          inver%*%x                               # function to obtain inverse of the matrix
-     }
-     list(set = set, get = get,
-          setinv = setinv,
-          getinv = getinv)
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
-## This is used to get cache data
-cacheSolve <- function(x, ...) {             
-     inv <- x$getinv()                       # return a matrix that is the inverse of 'x'
-     if(!is.null(inv)) {                     # checking whether inverse has already been calculated
-          message("getting cache data!")
-          return(inv)                        # get it from the cache and skips the computation then returns inverse value
-     }
-     data <- x$get()
-     inv <- solve(data,...)                  # otherwise, calculates inverse value
-     x$setinv(inv)
-     inv                                     # return a matrix that is the inverse of 'x'
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
